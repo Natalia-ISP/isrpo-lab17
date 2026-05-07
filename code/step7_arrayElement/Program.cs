8520eab8088490881232bb02834184077b2e11d4
natalia <ponomarevanatasha16072007@gmail.com>
2026-05-07 20:31:24 +0300

    Step 7 arrays

---
+++ code/step7_arrayElement/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("вывод массива");
+int[] num = [5, 4, 5, 3, 4];
+
+for (int i = 0; i < num.Length; i++)
+{
+    Console.WriteLine(num[i]);
+}
+Console.WriteLine("конец программы");
---
+++ code/step7_arrayMax/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] num = {5, 4, 5, 3, 4};
+
+int max_num = num[0];
+
+for (int i = 1; i < num.Length; i++)
+{
+    if (num[i] > max_num)
+    {
+        max_num = num[i];
+    }
+}
+Console.WriteLine($"макс элемент: {max_num}");
