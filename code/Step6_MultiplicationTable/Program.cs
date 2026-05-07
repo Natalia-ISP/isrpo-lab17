3d2a6081bcd43ec6726bba97477b80ccf36e0ebf
natalia <ponomarevanatasha16072007@gmail.com>
2026-05-07 19:07:04 +0300

    Step 6: while and for loops with flowcharts

---
+++ code/Step6_MultiplicationTable/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("таблица умножения");
+Console.Write("введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"таблица умножения для {number}");
+
+for (int i = 1; i <= 10; i++)
+{
+    int result = number * i;
+    Console.WriteLine($"{number} x {i} = {result}");
+}
+Console.WriteLine("конец программы");
---
+++ code/Step6_StairsClimb/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("подъем по лестнице");
+
+Console.Write("на какой этаж нужно подняться?: ");
+int targetFloor = Convert.ToInt32(Console.ReadLine());
+
+int currentFloor = 1;
+Console.WriteLine("начинаем подъем!");
+
+while (currentFloor < targetFloor)
+{
+    currentFloor++;
+    Console.WriteLine($"поднялись на {currentFloor}-й этаж");
+}
+Console.WriteLine($"вы достигли {targetFloor}-го этажа");
+Console.WriteLine("конец программы");
