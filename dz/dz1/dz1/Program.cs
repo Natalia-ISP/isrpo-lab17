1296f26089e50eedee50de14f80d7627124048c5
natalia <ponomarevanatasha16072007@gmail.com>
2026-05-13 11:59:38 +0300

    Completed Lab: Algorithms and Flowcharts

---
+++ dz/dz1/dz1/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("введите вес: ");
+double weight = Convert.ToDouble(Console.ReadLine());
+Console.Write("введите рост: ");
+double height = Convert.ToDouble(Console.ReadLine());
+double IMT = weight / (height*height);
+if (IMT < 18.5)
+{
+    Console.WriteLine("Недостаточный вес");
+    Console.WriteLine("Нужно есть больше");
+}
+else if (IMT < 24.9)
+{
+    Console.WriteLine("Нормальный вес");
+    Console.WriteLine("Все нормально");
+}
+else if (IMT < 29.9)
+{
+    Console.WriteLine("Избыточный вес");
+    Console.WriteLine("Нужно есть меньше");
+}
+else
+{
+    Console.WriteLine("Ожирение");
+    Console.WriteLine("Нужно есть меньше");
+}
---
+++ dz/dz3/Task3_GuessGame/Program.cs
@@ -0,0 +1,23 @@
+﻿Random random = new Random();
+int a = random.Next(1, 101);
+int n = 0;
+
+Console.WriteLine("угадайте число от 1 до 100: ");
+int b = 0;
+
+while (b != a)
+{
+    Console.Write("введите число: ");
+    b = Convert.ToInt32(Console.ReadLine());
+    n++;
+    if (b > a)
+    {
+        Console.WriteLine("число меньше");
+    }
+    else if (b < a)
+    {
+        Console.WriteLine("число больше");
+    }
+}
+Console.WriteLine("вы угадали");
+Console.WriteLine($"попыток: {n}");
